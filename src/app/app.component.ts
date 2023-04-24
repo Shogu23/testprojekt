b3388b951745fc8d7266ed2aea19979bbac5d365
Charles MARTHOURET <66626353+Shogu23@users.noreply.github.com>
2023-04-24 15:52:41 +0200

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Agent {
+  os: string;
+  lastKeepAlive: string;
+  dateAdd: string;
+  ip: string;
+  name: string;
+  id: string;
+  version: string;
+  status: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'agent-status-dashboard';
+export class AppComponent implements OnInit {
+  agents: Agent[] = [];
+  totalAgents = 0;
+  activeAgents = 0;
+  disconnectedAgents = 0;
+  pendingAgents = 0;
+  neverConnectedAgents = 0;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<{ results: Agent[] }>('../assets/agents.json')
+      .subscribe(response => {
+        // Filtrer l'agent avec l'ID 000 ou le nom cyr-customer-ossec.local
+        this.agents = response.results.filter(agent => agent.id !== '000' && agent.name !== 'cyr-customer-ossec.local');
+        this.totalAgents = this.agents.length;
+        this.activeAgents = this.agents.filter(agent => agent.status === 'active').length;
+        this.disconnectedAgents = this.agents.filter(agent => agent.status === 'disconnected').length;
+        this.pendingAgents = this.agents.filter(agent => agent.status === 'pending').length;
+        this.neverConnectedAgents = this.agents.filter(agent => agent.status === 'never_connected').length;
+      });
+  }
 }
